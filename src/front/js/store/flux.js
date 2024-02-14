6f97938243bc75e7f1fe17486481419097e533dc
Christa Kelly <christadkelly@gmail.com>
2024-02-14 21:10:24 +0000

    fetch todos

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,51 +1,28 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			userToDos: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			fetchUserToDos: async () => {
+				const opts = {
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+						'Access-Control-Allow-Origin': '*',
+						// 'Authorization': `Bearer ${sessionStorage.token}`
+					}
+				} 
+				try {
+					const resp = await fetch(`${process.env.BACKEND_URL}/api/todos`, opts);
+					const data = await resp.json();
+					console.log('fetch todos', data)
+					if (resp.status == 200) {
+						setStore({userToDos: data})
+					}
+				} catch (error) {
+					console.error(`There was a problem with the fetch operation ${error}`)
 				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
 		}
 	};
