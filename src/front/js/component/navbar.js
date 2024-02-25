bf16820f302b233e738722c47912f69824fe9fe4
Christa Kelly <christadkelly@gmail.com>
2024-02-25 23:42:18 +0000

    navbar done

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,16 +3,36 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light border border-secondary-subtle border-3">
+			<div className="container-fluid">
+				<Link to={"/"} className="navbar-brand">
+					miaa
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      				<span className="navbar-toggler-icon"></span>
+    			</button>
+    			<div className="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+      				<ul className="navbar-nav">
+						<li className="nav-item d-flex">
+							<i className="fa-solid fa-thumbtack"></i>
+							<Link className="nav-link ms-2" to={"/contacts"}>Contacts</Link>
+						</li>
+						<li className="nav-item d-flex">
+							<i className="fa-solid fa-thumbtack"></i>
+							<Link className="nav-link ms-2" to={"/todos"}>To-Do List</Link>
+						</li>
+						<li className="nav-item dropdown d-flex">
+							<i className="fa-solid fa-thumbtack"></i>
+    	    				<a className="nav-link dropdown-toggle ms-2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            					Settings
+          					</a>
+          					<ul className="dropdown-menu">
+            					<li><a className="dropdown-item" href="#">Account Settings</a></li>
+            					<li><a className="dropdown-item" href="#">Logout</a></li>
+          					</ul>
+        				</li>
+      				</ul>
+    			</div>
 			</div>
 		</nav>
 	);
