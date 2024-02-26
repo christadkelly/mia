d3fb43eae554b19a56867d47812c0ec80206d68a
Christa Kelly <christadkelly@gmail.com>
2024-02-26 21:45:18 +0000

    memos mapping

--- src/front/js/component/memo.js
+++ src/front/js/component/memo.js
@@ -2,12 +2,15 @@ import React, { useState, useContext } from 'react'
 import { Context } from '../store/appContext'
 import { useNavigate } from "react-router-dom";
 
-export const Contact = (props) => {
+export const Memo = (props) => {
     const {store, actions} = useContext(Context);
     const nagivate = useNavigate();
     const memo = props.memo;
 
     return(
-        <div>hi</div>
+        <div>
+            <h6>{memo.title}</h6>
+            <p>{memo.memo_body}</p>
+        </div>
     )
 }
--- src/front/js/pages/memoList.js
+++ src/front/js/pages/memoList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { Memo } from "../component/memo";
 
 export const MemoList = () => {
     const { store, actions } = useContext(Context);
@@ -9,9 +10,13 @@ export const MemoList = () => {
         actions.fetchUserMemos()
     }, [])
 
+    console.log(store.userMemos)
+
     return(
         <div>
-            hi
+            {store.userMemos && store.userMemos.length > 0 && store.userMemos.map((memo, key) => {
+                return <Memo key={key} memo={memo}/>
+            })}
         </div>
     )
 }
